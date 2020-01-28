8c6f14067a5341e744b045a7cd807bf175375999
Shannen Grimes <shannengrimes@yahoo.com>
2020-01-27 22:58:03 -0600

    Added handlebars

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,53 @@
+// Required 
+var express = require("express");
+// Import the burger models
+var burger = require("../models/burger.js");
+
+// Create the router for the app, and export the router at the end of your file.
+var router = express.Router();
+// Create routes
+router.get("/", function (req, res) {
+    burger.selectAll(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+// Add new burger to the database
+router.post("/api/burgers", function (req, res) {
+    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+        // Send back the ID of the new burger
+        res.json({ id: result.insertId });
+    });
+});
+// Set burger devoured status to true
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.updateOne({ devoured: req.body.devoured }, condition, function(result) {
+        if (result.changedRows === 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+// Delete burger from database
+router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
+
+    burger.deleteOne(condition, function(result) {
+        if (result.changedRows === 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
